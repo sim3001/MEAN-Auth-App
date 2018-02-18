94a726a556d75b4e72bf11c22aecab6347378b77
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-18 14:14:26 -0700

    Setup app.js middleware and Server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const users = require('./routes/users')
+
+const port = 3000;
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/users', users);
+
+//index route
+app.get('/', (req,res) => {
+    res.send('Invalid Endpoint');
+})
+//setup server
+app.listen(port, () =>{
+    console.log(`The application is running on port ${port}`);
+})
