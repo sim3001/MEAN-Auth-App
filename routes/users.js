83bdb624837320e1abc9b53ffc0ef01b8801ddfe
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-19 15:31:06 -0700

    Register route working now
    Register route works as expected with password hashing etc... Also fixed a few syntactical errors

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
 
 //Register Route
 router.post('/register',(req,res,next)=>{
-    res.send('Register Page');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err,user)=>{
+        if(err){
+            res.json({success:false, msg:'Fail to register user'});
+        }else{
+            res.json({success:true, msg:'User registered!'});
+        }
+    });
 });
 //Authentication Route
 router.post('/authenticate',(req,res,next)=>{
