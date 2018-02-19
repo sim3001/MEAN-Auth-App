5e054eb890503c690b7de96058fc6224af70c6de
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-19 14:59:45 -0700

    Added User models

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Register Route
-router.get('/register',(req,res,next)=>{
+router.post('/register',(req,res,next)=>{
     res.send('Register Page');
 });
 //Authentication Route
@@ -13,9 +13,6 @@ router.post('/authenticate',(req,res,next)=>{
 router.get('/profile',(req,res,next)=>{
     res.send('Profile Page');
 });
-//Validate Route
-router.get('/validate',(req,res,next)=>{
-    res.send('Validate Page');
-});
+
 
 module.exports = router;
