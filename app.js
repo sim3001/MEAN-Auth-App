e9e38b7afac306915f34e6edc7c9bac6b9bc2d61
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-23 17:30:28 -0700

    Fixing routes issue

--- app.js
+++ app.js
@@ -6,28 +6,46 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-//connect to database
-mongoose.connect(config.database);
-//On connection
+// Connect To Database (NEW) But not working!!!!!!!!!! (because of secret in db.js!!!!!)
+//const db = require('./config/database');
+// Map global promise - get rid of warning
+//mongoose.Promise = global.Promise;
+// Connect to mongoose
+//mongoose.connect(db.mongoURI, {
+//useMongoClient: true
+//})
+//.then(() => console.log('MongoDB Connected...'))
+//.catch(err => console.log(err));
+
+
+// Connect To Database (OLD CODE)
+mongoose.connect(config.database, { useMongoClient: true});
+// On Connection
 mongoose.connection.on('connected', () => {
-    console.log(`Connected to database ${config.database}`);
+    console.log('Connected to Database '+config.database);
 });
-//On Error
-mongoose.connection.on('error', (error) => {
-    console.log(`Database error ${error}`);
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+err);
 });
 
 const app = express();
 
-const users = require('./routes/users.js');
+const users = require('./routes/users');
 
+// Port Number
 const port = process.env.PORT || 8080;
 
-//middleware
+// CORS Middleware
 app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-//Passport middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -35,19 +53,16 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-//set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//index route
+// Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+    res.send('invaild endpoint');
 });
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-//setup server
+// Start Server
 app.listen(port, () => {
-    console.log(`The application is running on port ${port}`);
+    console.log('Server started on port '+port);
 });
