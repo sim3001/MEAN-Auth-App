86d92e69febd78f93bbf5d0c2a99489d71f55bb8
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-23 17:37:50 -0700

    New build

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://<sim>:<sim>@ds147668.mlab.com:47668/meanauthappproject',
+    database: "mongodb://<sim>:<sim>@ds147668.mlab.com:47668/meanauthappproject",//prod
+    //database: 'mongodb://localhost:27017/meanauth',    //dev
     secret: 'yoursecret'
 };
