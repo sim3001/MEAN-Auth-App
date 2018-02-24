a0da5e8a33daac46882dc5db993403e094c169a8
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-23 18:21:22 -0700

    Trying to make Backend work on heroku

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,33 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import { HttpModule } from '@angular/http';
+import { Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { tokenNotExpired } from 'angular2-jwt';
+import {tokenNotExpired} from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
   authToken: any;
   user: any;
-  isDev: any;
-
-  constructor(private http: Http) {
-    this.isDev = false;  // Change to false before deployment
-  }
+  constructor(private http:Http) { }
 
   registerUser(user) {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post('users/register', user, {headers: headers})
-      .map(res => res.json());
+    let headers= new Headers();
+    headers.append('Content-type', 'application/json');
+    return this.http.post('users/register', user, {headers:headers}).map(res => res.json());
   }
-
   authenticateUser(user) {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    return this.http.post('users/authenticate', user, {headers: headers})
-      .map(res => res.json());
+    let headers= new Headers();
+    headers.append('Content-type', 'application/json');
+    return this.http.post('users/authenticate', user, {headers:headers}).map(res => res.json());
   }
-
   getProfile() {
     let headers = new Headers();
     this.loadToken();
@@ -37,18 +28,16 @@ export class AuthService {
       .map(res => res.json());
   }
 
-  storeUserData(token, user) {
-    localStorage.setItem('id_token', token);
-    localStorage.setItem('user', JSON.stringify(user));
+  storeUserData(token, user){
+    localStorage.setItem('id_token',token);
+    localStorage.setItem('user',JSON.stringify(user));
     this.authToken = token;
     this.user = user;
   }
-
   loadToken() {
     const token = localStorage.getItem('id_token');
     this.authToken = token;
   }
-
   loggedIn() {
     return tokenNotExpired('id_token');
   }
