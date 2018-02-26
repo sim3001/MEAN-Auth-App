4d31400b3d9d684119c28d2de3f0ce071893fcca
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-25 21:08:22 -0700

    fixed last commit

--- angular-src/src/app/components/floppybird/floppybird.component.ts
+++ angular-src/src/app/components/floppybird/floppybird.component.ts
@@ -77,55 +77,54 @@ export class FloppybirdComponent implements OnInit {
       }
 
       function draw() {
+        if(run === true) {
+          ctx.drawImage(bg, 0, 0);
 
-        ctx.drawImage(bg, 0, 0);
+
+          for (var i = 0; i < pipe.length; i++) {
+
+            constant = pipeNorth.height + gap;
+            ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+            ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+
+            pipe[i].x--;
+
+            if (pipe[i].x == 125) {
+              pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+              });
+            }
+
+            // detect collision
+
+            if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+              location.reload();
+            }
+
+            if (pipe[i].x == 5) {
+              score++;
+              scor.play();
+            }
 
 
-        for (var i = 0; i < pipe.length; i++) {
-
-          constant = pipeNorth.height + gap;
-          ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-          ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
-
-          pipe[i].x--;
-
-          if (pipe[i].x == 125) {
-            pipe.push({
-              x: cvs.width,
-              y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-            });
           }
 
-          // detect collision
+          ctx.drawImage(fg, 0, cvs.height - fg.height);
 
-          if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
-            location.reload();
-          }
+          ctx.drawImage(bird, bX, bY);
 
-          if (pipe[i].x == 5) {
-            score++;
-            scor.play();
-          }
+          bY += gravity;
 
+          ctx.fillStyle = "#000";
+          ctx.font = "20px Verdana";
+          ctx.fillText("Score : " + score, 10, cvs.height - 20);
 
+          requestAnimationFrame(draw);
         }
-
-        ctx.drawImage(fg, 0, cvs.height - fg.height);
-
-        ctx.drawImage(bird, bX, bY);
-
-        bY += gravity;
-
-        ctx.fillStyle = "#000";
-        ctx.font = "20px Verdana";
-        ctx.fillText("Score : " + score, 10, cvs.height - 20);
-
-        requestAnimationFrame(draw);
-
       }
-    if(run===true) {
+
       draw();
-    }
   }
   }
 
