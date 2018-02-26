e1e6c6cfc728fd1736028a32bca5920317e19800
Simranjit Singh <simranjit.boyal@gmail.com>
2018-02-25 20:06:40 -0700

    revert

--- angular-src/src/app/components/forgot/forgot.component.ts
+++ angular-src/src/app/components/forgot/forgot.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ValidateService } from '../../services/validate.service';
-import { AuthService } from '../../services/auth.service';
-import { FlashMessagesService } from 'angular2-flash-messages';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot',
@@ -10,34 +6,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent implements OnInit {
-  email: String;
 
-  constructor(
-    private validateService: ValidateService,
-    private flashMessage: FlashMessagesService,
-    private authService: AuthService,
-    private router:Router
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
-  onforgotSubmit(){
-  const user = {
-    email : this.email.toLowerCase()
-  }
-  this.authService.forgotPassword(user).subscribe(data =>{
-      if(data.success){
 
-      this.flashMessage.show(data.msg, {cssClass:'alert-success text-center',
-      timeout:5000});
-
-      this.router.navigate(['/']);
-
-    } else {
-      this.flashMessage.show(data.msg, {cssClass:'alert-danger text-center',
-      timeout:5000});
-      this.router.navigate(['forgot']);
-    }
-  });
-  }
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from "@angular/core";
-import { Http, Headers } from "@angular/http";
-import "rxjs/add/operator/map";
-import { tokenNotExpired } from "angular2-jwt";
+import { Injectable } from '@angular/core';
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {tokenNotExpired} from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
   authToken: any;
   user: any;
-  constructor(private http: Http) {}
+  constructor(private http:Http) { }
 
   registerUser(user) {
-<<<<<<< HEAD
     let headers= new Headers();
     headers.append('Content-type', 'application/json');
     return this.http.post('users/register', user, {headers:headers}).map(res => res.json());
@@ -19,69 +18,28 @@ export class AuthService {
     let headers= new Headers();
     headers.append('Content-type', 'application/json');
     return this.http.post('users/authenticate', user, {headers:headers}).map(res => res.json());
-=======
-    let headers = new Headers();
-    headers.append("Content-type", "application/json");
-    return this.http
-      .post("http://localhost:3000/users/register", user, { headers: headers })
-      .map(res => res.json());
   }
-  authenticateUser(user) {
-    let headers = new Headers();
-    headers.append("Content-type", "application/json");
-    return this.http
-      .post("http://localhost:3000/users/authenticate", user, {
-        headers: headers
-      })
-      .map(res => res.json());
-  }
-  forgotPassword(user) {
-    let headers = new Headers();
-    headers.append("Content-type", "application/json");
-    return this.http
-      .post("http://localhost:3000/users/forgot", user, { headers: headers })
-      .map(res => res.json());
-  }
-
-  resetPassword(user) {
-    let headers = new Headers();
-    headers.append("Content-type", "application/json");
-    return this.http
-      .post(`http://localhost:3000/users/reset/${user.token}`, user, {
-        headers: headers
-      })
-      .map(res => res.json());
->>>>>>> passwordreset
-  }
-
   getProfile() {
     let headers = new Headers();
     this.loadToken();
-<<<<<<< HEAD
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
     return this.http.get('users/profile', {headers: headers})
-=======
-    headers.append("Authorization", this.authToken);
-    headers.append("Content-Type", "application/json");
-    return this.http
-      .get("http://localhost:3000/users/profile", { headers: headers })
->>>>>>> passwordreset
       .map(res => res.json());
   }
 
-  storeUserData(token, user) {
-    localStorage.setItem("id_token", token);
-    localStorage.setItem("user", JSON.stringify(user));
+  storeUserData(token, user){
+    localStorage.setItem('id_token',token);
+    localStorage.setItem('user',JSON.stringify(user));
     this.authToken = token;
     this.user = user;
   }
   loadToken() {
-    const token = localStorage.getItem("id_token");
+    const token = localStorage.getItem('id_token');
     this.authToken = token;
   }
   loggedIn() {
-    return tokenNotExpired("id_token");
+    return tokenNotExpired('id_token');
   }
 
   logout() {
